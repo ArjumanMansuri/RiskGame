636862fc38e640505a956763cc71fad45af9cc18
goutham <goumis7.urs@gmail.com>
2019-10-17 03:26:33 -0400

    Added Test case for calculation of number of reinforcement armies

--- src/com/riskTest/controller/ReinforcementTest.java
+++ src/com/riskTest/controller/ReinforcementTest.java
@@ -1,5 +1,50 @@
 package com.riskTest.controller;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+
+import org.junit.*;
+
+import com.riskGame.controller.*;
+import com.riskGame.models.*;
+
+
+/**
+ * {@link ReinforcementPhase} Test Class
+ */
 public class ReinforcementTest {
 
+	ReinforcementPhase reinforcement;
+	Continent myContinent;
+	Player myPlayer;
+	
+	
+	@Before
+	public void before() {
+		
+	myPlayer = new Player();
+	HashMap<String, Country> ownedCountries = new HashMap<String, Country>();
+	String[] countries = {"Iran", "Japan", "canada","Germany", "France", "Asia", "Siberia", "China", "Afghanistan", "Ukraine", "Yatusk", "Kamchatka", "Mongolia", "Egypt", "Indonesia"};
+	for(String countryName: countries) {
+		Country newCountry = new Country();
+		newCountry.setCountryName(countryName);
+		ownedCountries.put(countryName, newCountry);
+	}
+	myPlayer.setOwnedCountries(ownedCountries);
+		
+	}
+	
+	/**
+	 * This method is to test number of reinforcement armies of Player.
+	 * Calculation : reinforcement armies = Number of territory owned by Player / 3.
+	 * e.g Number of Territory owned by Player : 15
+	 * 	   Reinforcement Armies  = 15/3 = 5 new armies
+	 */
+	@Test
+	public void testGenerateReinforcementArmy() {
+		
+		reinforcement = new ReinforcementPhase();
+		assertEquals(5,reinforcement.calculateReinforcementArmies(myPlayer));	
+	}
 }
