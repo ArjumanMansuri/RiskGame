f3d9b68feaba78ead88bf97fbef445fb1e53d4f7
Niral H Lad <43843579+niralhlad@users.noreply.github.com>
2019-10-17 07:18:54 -0400

    Game and map debugged

--- src/com/riskGame/models/Game.java
+++ src/com/riskGame/models/Game.java
@@ -10,48 +10,88 @@ import java.util.HashMap;
  */
 public class Game {
 
-    /**
-     * @param map Refers to the map used for the game play
-     * @param playersList Refers to the all the players of a particular game
-     */
     private static Map map = new Map();
     private static HashMap<Integer,Player> playersList = new HashMap<>();
     private static Map editMap = new Map();
     private static boolean editMapSet = false;
 
+    /**
+     * This is a default constructor that creates game object.
+     *
+     */
     public Game() {
         playersList = new HashMap<Integer, Player>();
         map = new Map();
     }
 
+    /**
+     * getter method to get static object of map from the map model.
+     * @return map object.
+     *
+     */
     public static Map getMap() {
         return map;
     }
 
+    /**
+     * setter method to set static object of map from the map model.
+     * @param map set map object.
+     *
+     */
     public static void setMap(Map map) {
         Game.map = map;
     }
 
+    /**
+     * getter method to get static object list of players.
+     * @return playersList list of players.
+     *
+     */
     public static HashMap<Integer, Player> getPlayersList() {
         return playersList;
     }
 
+    /**
+     * setter method to set list of players.
+     * @param playersList hashmap of players.
+     *
+     */
     public static void setPlayersList(HashMap<Integer, Player> playersList) {
         Game.playersList = playersList;
     }
 
+    /**
+     * getter method to get static object edited map.
+     * @return map object of the edited map.
+     *
+     */
     public static Map getEditMap() {
         return editMap;
     }
 
+    /**
+     * setter method to set static object edited map.
+     * @param editMap object for the edited map.
+     *
+     * */
     public static void setEditMap(Map editMap) {
         Game.editMap = editMap;
     }
 
+    /**
+     * checks if the edited map is set into file.
+     * @return true if edited map is set correctly.
+     *
+     */
     public static boolean isEditMapSet() {
         return editMapSet;
     }
 
+    /**
+     * setter method to set the edited map to true if correct.
+     * @param editMapSet passese the boolean value true if set correctly.
+     *
+     */
     public static void setEditMapSet(boolean editMapSet) {
         Game.editMapSet = editMapSet;
     }
--- src/com/riskGame/models/Map.java
+++ src/com/riskGame/models/Map.java
@@ -2,17 +2,36 @@ package com.riskGame.models;
 
 import java.util.HashMap;
 
+/**
+ * This is a model class for Map that holds the properties of map.
+ * @author goumis
+ *
+ */
+
 public class Map {
 	private HashMap<String, Continent> continents;
 
+	/**
+	 * This is a default constructor that will create Map object.
+	 *
+	 */
 	public Map() {
 		continents = new HashMap<String, Continent>();
 	}
-	
+
+	/**
+	 * getter method to get list of continents.
+	 * @return hashmap of continents.
+	 *
+	 */
 	public HashMap<String, Continent> getContinents() {
 		return continents;
 	}
 
+	/**
+	 * setter method to set list of continents.
+	 * @param continents hashmap of continents.
+	 */
 	public void setContinents(HashMap<String, Continent> continents) {
 		this.continents = continents;
 	}
