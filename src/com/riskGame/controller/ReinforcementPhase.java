228780ce24c5a95f005076ae945f6e2066ad7346
emnsrjm <arjuman.mansuri@ericsson.com>
2019-10-17 06:45:34 -0400

    Bug Fixes in Reinforcement Phase

--- src/com/riskGame/controller/ReinforcementPhase.java
+++ src/com/riskGame/controller/ReinforcementPhase.java
@@ -5,35 +5,42 @@ import com.riskGame.models.Game;
 import com.riskGame.models.Player;
 
 public class ReinforcementPhase {
-	public String reinforce(int player,String command) {		
-		Player currentPlayer = Game.getPlayersList().get(player);
+	
+	public void calculateReinforcementArmies(int i) {
+		Player p = Game.getPlayersList().get(i);
+		int newArmies =  p.getOwnedCountries().size() /3;
+		p.setPlayerNumOfArmy(newArmies);
+	}
+	
+	public String reinforce(int player,String command) {	
 		
-		// command validation 
-		String[] commandValidate = command.split(" ");
-		if(commandValidate.length != 3 || !commandValidate[0].equals("reinforce")) {
-			return "error";
+		if(command.isEmpty() || command.trim().length()==0) return "Error : Invalid Command";
+		//check if it is a reinforcement command
+		String[] commandComponents = command.split(" ");
+		if(!(commandComponents.length == 3)) {
+			return "Error : Number of arguments does not match";
 		}
 		
-		String countryName = commandValidate[1];
-		int numberOfArmies = Integer.parseInt(commandValidate[2]);
+		String commandName = commandComponents[0];
+		if(!commandName.equalsIgnoreCase("reinforce")) {
+			return "Error : Please enter reinforcement command";
+		}
+		String countryName = commandComponents[1];
+		int num = Integer.parseInt(commandComponents[2]);
+		// reinforce 'countryName' 'num'
 		
-		// country validation
-		if(currentPlayer.getOwnedCountries().containsKey(countryName)) {
-			int currentArmies = currentPlayer.getOwnedCountries().get(countryName).getNumberOfArmies();
-			int newArmies = calculateReinforcementArmies(currentPlayer);		
-			
-			if(numberOfArmies <= newArmies) {
-				currentArmies += newArmies;
-				currentPlayer.getOwnedCountries().get(countryName).setNumberOfArmies(currentArmies);
-				currentPlayer.setPlayerNumOfArmy(currentPlayer.getPlayerNumOfArmy() + newArmies);
-				return "saved";
-			}			
-		} 
-		return "error";				
-	}
-
-	public int calculateReinforcementArmies(Player currentPlayer) {
-		int reinformentArmies =  currentPlayer.getOwnedCountries().size() /3;
-		return reinformentArmies;
+		
+			Player p = Game.getPlayersList().get(player);
+			if(!p.getOwnedCountries().containsKey(countryName)) {
+				return "Error : Country not owned by "+p.getPlayerName()+" or invalid country ";
+			}
+			if(!(p.getPlayerNumOfArmy()>=num)) {
+				return "Error : Insufficient armies to move";
+			}
+			p.setPlayerNumOfArmy(p.getPlayerNumOfArmy()-num);
+			p.getOwnedCountries().get(countryName).setNumberOfArmies(p.getOwnedCountries().get(countryName).getNumberOfArmies()+num);
+		
+		
+		return "";			
 	}
 }
