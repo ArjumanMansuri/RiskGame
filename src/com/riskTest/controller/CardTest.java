48191861729c704fc56f9804f5ae281e388a9f2b
Sharan <chitta.sharan@gmail.com>
2019-11-06 04:12:50 -0500

    Card Test added player num

--- src/com/riskTest/controller/CardTest.java
+++ src/com/riskTest/controller/CardTest.java
@@ -9,14 +9,34 @@ import com.riskGame.models.*;
 
 public class CardTest {
 	
+	Player myPlayer;
 	@Before
 	public void before() {
-		
+		myPlayer = new Player();
 	}
 	
 	@Test
-	public void testGenerateReinforcementArmy() {
+	public void testCardType() {
 		Card card1 = new Card(Card.ARTILLARY);
 		assertEquals(Card.ARTILLARY,card1.getType());	
 	}
+	
+	@Test
+	public void testCardOwner() {
+		Card card1 = new Card(Card.ARTILLARY);
+		card1.changeOwner(myPlayer);		
+		assertEquals(card1.getOwner(), myPlayer);			
+	}
+	
+	@Test
+	public void testCardPlayerNums() {
+		Card card1 = new Card(Card.ARTILLARY);
+		Card card2 = new Card(Card.ARTILLARY);
+		Card card3 = new Card(Card.ARTILLARY);
+		card1.changeOwner(myPlayer);		
+		card2.changeOwner(myPlayer);		
+		card3.changeOwner(myPlayer);		
+		assertEquals(3, myPlayer.getCards().size());			
+	}
+	
 }
