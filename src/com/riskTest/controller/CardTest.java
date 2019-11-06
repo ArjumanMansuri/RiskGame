45820eb915dc4684572e8c5e0eb53f1a1fcb377c
Sharan <chitta.sharan@gmail.com>
2019-11-06 04:22:38 -0500

    All the test cases for Card

--- src/com/riskTest/controller/CardTest.java
+++ src/com/riskTest/controller/CardTest.java
@@ -15,12 +15,18 @@ public class CardTest {
 		myPlayer = new Player();
 	}
 	
+	/**
+	 * Test if the card type is correctly set. 
+	 */
 	@Test
 	public void testCardType() {
 		Card card1 = new Card(Card.ARTILLARY);
 		assertEquals(Card.ARTILLARY,card1.getType());	
 	}
 	
+	/**
+	 * Test if the card owner is correctly set.
+	 */
 	@Test
 	public void testCardOwner() {
 		Card card1 = new Card(Card.ARTILLARY);
@@ -28,6 +34,9 @@ public class CardTest {
 		assertEquals(card1.getOwner(), myPlayer);			
 	}
 	
+	/**
+	 * Test the number of cards owned by the player object.
+	 */
 	@Test
 	public void testCardPlayerNums() {
 		Card card1 = new Card(Card.ARTILLARY);
@@ -39,6 +48,9 @@ public class CardTest {
 		assertEquals(3, myPlayer.getCards().size());			
 	}
 	
+	/**
+	 * Test if the card gets transferred
+	 */
 	@Test
 	public void testCardTransfer() {
 		Player myPlayer2 = new Player();
@@ -49,7 +61,7 @@ public class CardTest {
 		card1.changeOwner(myPlayer);		
 		card1.changeOwner(myPlayer2); // change the owner of the card 
 		
-		assertEquals("Player2", myPlayer.getPlayerName());			
+		assertEquals("Player2", myPlayer2.getPlayerName());			
 	}
 	
 }
