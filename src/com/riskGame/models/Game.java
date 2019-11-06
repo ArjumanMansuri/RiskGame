f26c37a82bfddc0c948bbef56db6c942df16181f
Mudra-PC <mudraparikh19@gmail.com>
2019-11-06 01:48:19 -0500

    Adding JavaDocs

--- src/com/riskGame/models/Game.java
+++ src/com/riskGame/models/Game.java
@@ -16,155 +16,148 @@ import com.riskGame.view.CardExchange;
  */
 public class Game {
 
-    private static Map map = new Map();
-    private static HashMap<Integer,Player> playersList = new HashMap<>();
-    private static Map editMap = new Map();
-    private static boolean editMapSet = false;
-    private static ArrayList<Card> gameCards;
-    private static CardExchange cardExView = new CardExchange();
-    public static int exchanges_made = 0;
-    
-    /**
-     * This is a default constructor that creates game object.
-     *
-     */
-    public Game() {
-        playersList = new HashMap<Integer, Player>();
-        map = new Map();
-    }
-    
-    public static void initCards() {
-    	gameCards = new ArrayList<Card>();
-    	
-    	for(int i = 0; i < 14; i++) {
-    		Card c = new Card(Card.ARTILLARY);
-    		gameCards.add(c);
-    	}
-    	
-    	for(int i = 0; i < 14; i++) {
-    		Card c = new Card(Card.CAVALRY);
-    		gameCards.add(c);
-    	}
-    	
-    	for(int i = 0; i < 14; i++) {
-    		Card c = new Card(Card.INFANTRY);
-    		gameCards.add(c);
-    	}
-    }
-    
-    public static ArrayList<Card> getCards() {
-    	return gameCards;
-    }
-    
-    public static void assignRandomCard(Player p) {
-    	if(gameCards != null) {
-        	Collections.shuffle(gameCards);    		
-    	}
-    	for(Card c : gameCards) {
-    		if(c.getOwner() == null) {
-    			c.changeOwner(p);
-    			return;
-    		}
-    	}
-    }
+	private static Map map = new Map();
+	private static HashMap<Integer,Player> playersList = new HashMap<>();
+	private static Map editMap = new Map();
+	private static boolean editMapSet = false;
+	private static ArrayList<Card> gameCards;
+	private static CardExchange cardExView = new CardExchange();
+	public static int exchanges_made = 0;
 
-    /**
-     * getter method to get static object of map from the map model.
-     * @return map object.
-     *
-     */
-    public static Map getMap() {
-        return map;
-    }
+	/**
+	 * This is a default constructor that creates game object.
+	 *
+	 */
+	public Game() {
+		playersList = new HashMap<Integer, Player>();
+		map = new Map();
+	}
 
-    /**
-     * setter method to set static object of map from the map model.
-     * @param map set map object.
-     *
-     */
-    public static void setMap(Map map) {
-        Game.map = map;
-    }
+	/**
+	 * This is the method to assign different types of cards
+	 */
+	public static void initCards() {
+		gameCards = new ArrayList<Card>();
 
-    /**
-     * getter method to get static object list of players.
-     * @return playersList list of players.
-     *
-     */
-    public static HashMap<Integer, Player> getPlayersList() {
-        return playersList;
-    }
+		for(int i = 0; i < 14; i++) {
+			Card c = new Card(Card.ARTILLARY);
+			gameCards.add(c);
+		}
 
-    /**
-     * setter method to set list of players.
-     * @param playersList hashmap of players.
-     *
-     */
-    public static void setPlayersList(HashMap<Integer, Player> playersList) {
-        Game.playersList = playersList;
-        
-        initCards();
-        for(Integer key : Game.playersList.keySet()) {
-        	if(cardExView == null) {
-        		System.out.println("EX VIEW IS NULL");
-        		continue;
-        	}
-        	Game.playersList.get(key).addObserver(cardExView);
-        }
-        
-        /*
-         * For Testing
-        
-        if(Game.playersList.size() > 1) {
-        	// Bug the playerlist is not correct. First player is null. Second is the one u pass as first
-	        Game.gameCards.get(0).changeOwner(Game.playersList.get(1));
-	        Game.gameCards.get(15).changeOwner(Game.playersList.get(1));
-	        Game.gameCards.get(30).changeOwner(Game.playersList.get(1));
-	        
-	        Game.gameCards.get(3).changeOwner(Game.playersList.get(2));
-	        Game.gameCards.get(4).changeOwner(Game.playersList.get(2));
-	        Game.gameCards.get(5).changeOwner(Game.playersList.get(2));
-	        
-	        //ReinforcementPhase rp = new ReinforcementPhase();
-	        //rp.reinforce(1, "exchangecards -none");
-	        //rp.reinforce(1, "reinforce ");
-        }*/
-    }
+		for(int i = 0; i < 14; i++) {
+			Card c = new Card(Card.CAVALRY);
+			gameCards.add(c);
+		}
 
-    /**
-     * getter method to get static object edited map.
-     * @return map object of the edited map.
-     *
-     */
-    public static Map getEditMap() {
-        return editMap;
-    }
+		for(int i = 0; i < 14; i++) {
+			Card c = new Card(Card.INFANTRY);
+			gameCards.add(c);
+		}
+	}
 
-    /**
-     * setter method to set static object edited map.
-     * @param editMap object for the edited map.
-     *
-     * */
-    public static void setEditMap(Map editMap) {
-        Game.editMap = editMap;
-    }
+	/**
+	 * getter method to get static arrayList of cards
+	 * @return list of cards 
+	 */
+	public static ArrayList<Card> getCards() {
+		return gameCards;
+	}
 
-    /**
-     * checks if the edited map is set into file.
-     * @return true if edited map is set correctly.
-     *
-     */
-    public static boolean isEditMapSet() {
-        return editMapSet;
-    }
+	/**
+	 * method to assign cards randomly among players
+	 * @param p player
+	 */
+	public static void assignRandomCard(Player p) {
+		if(gameCards != null) {
+			Collections.shuffle(gameCards);    		
+		}
+		for(Card c : gameCards) {
+			if(c.getOwner() == null) {
+				c.changeOwner(p);
+				return;
+			}
+		}
+	}
 
-    /**
-     * setter method to set the edited map to true if correct.
-     * @param editMapSet passese the boolean value true if set correctly.
-     *
-     */
-    public static void setEditMapSet(boolean editMapSet) {
-        Game.editMapSet = editMapSet;
-    }
+	/**
+	 * getter method to get static object of map from the map model.
+	 * @return map object.
+	 *
+	 */
+	public static Map getMap() {
+		return map;
+	}
+
+	/**
+	 * setter method to set static object of map from the map model.
+	 * @param map set map object.
+	 *
+	 */
+	public static void setMap(Map map) {
+		Game.map = map;
+	}
+
+	/**
+	 * getter method to get static object list of players.
+	 * @return playersList list of players.
+	 *
+	 */
+	public static HashMap<Integer, Player> getPlayersList() {
+		return playersList;
+	}
+
+	/**
+	 * setter method to set list of players.
+	 * @param playersList hashmap of players.
+	 *
+	 */
+	public static void setPlayersList(HashMap<Integer, Player> playersList) {
+		Game.playersList = playersList;
+
+		initCards();
+		for(Integer key : Game.playersList.keySet()) {
+			if(cardExView == null) {
+				System.out.println("EX VIEW IS NULL");
+				continue;
+			}
+			Game.playersList.get(key).addObserver(cardExView);
+		}
+	}
+
+	/**
+	 * getter method to get static object edited map.
+	 * @return map object of the edited map.
+	 *
+	 */
+	public static Map getEditMap() {
+		return editMap;
+	}
+
+	/**
+	 * setter method to set static object edited map.
+	 * @param editMap object for the edited map.
+	 *
+	 * */
+	public static void setEditMap(Map editMap) {
+		Game.editMap = editMap;
+	}
+
+	/**
+	 * checks if the edited map is set into file.
+	 * @return true if edited map is set correctly.
+	 *
+	 */
+	public static boolean isEditMapSet() {
+		return editMapSet;
+	}
+
+	/**
+	 * setter method to set the edited map to true if correct.
+	 * @param editMapSet passese the boolean value true if set correctly.
+	 *
+	 */
+	public static void setEditMapSet(boolean editMapSet) {
+		Game.editMapSet = editMapSet;
+	}
 }
 
